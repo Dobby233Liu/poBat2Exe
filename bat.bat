7969de9e5fb5b6520a90c9311fbcaf5a809537ce
Liu-Wen-Yuan <15816141883@163.com>
2017-05-28 02:43:05 -0500

    Update bat.bat

--- bat.bat
+++ bat.bat
@@ -1,7 +1,9 @@
-@echo of
+@echo off
 color 0a
 title bat2exe
-Pause
-goto convert
+set var=
+set /p var=Confirm (y / n)
+IF NOT \"%var%\"==y\"\" exit
+IF \"%var%\"==y\"\" goto convert
 :convert
 *REN .bat .exe
